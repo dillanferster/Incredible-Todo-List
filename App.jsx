3b63ac2cef2ca515d72860a74807fcc311fa19ea
Dillan <dillyferster@gmail.com>
2024-01-29 19:30:15 -0700

    Lab 2 - Layout components

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ToDoList from './ToDoList';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
+import ToDoForm from './ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
